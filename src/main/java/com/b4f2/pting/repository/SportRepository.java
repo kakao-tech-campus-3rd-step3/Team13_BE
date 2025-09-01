97adf5beec46554c1a1dae555a4d5effbf832597
Yunseong <dev.yunseong@gmail.com>
2025-09-01 20:04:41 +0900

    feat(Sport): sport의 domain, repository 구현

---
+++ src/main/java/com/b4f2/pting/repository/SportRepository.java
@@ -0,0 +1,7 @@
+package com.b4f2.pting.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SportRepository extends JpaRepository<SportRepository, Long> {
+
+}
