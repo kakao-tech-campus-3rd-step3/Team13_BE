614af915dfe1c14611213a4052b7899fcad74544
Yunseong <dev.yunseong@gmail.com>
2025-09-01 20:04:50 +0900

    feat(Game): game의 domain, repository 구현

---
+++ src/main/java/com/b4f2/pting/domain/Game.java
@@ -0,0 +1,45 @@
+package com.b4f2.pting.domain;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "game")
+@NoArgsConstructor
+public class Game {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "sport_id")
+    private Sport sport;
+
+    @Column(name = "game_status")
+    @Enumerated(EnumType.STRING)
+    private GameStatus gameStatus;
+
+    @Column(name = "start_time")
+    private LocalDateTime startTime;
+
+    @Column(name = "duration")
+    private Integer duration;
+
+    public enum GameStatus {
+        ON_MATCHING,
+        END
+    }
+}
