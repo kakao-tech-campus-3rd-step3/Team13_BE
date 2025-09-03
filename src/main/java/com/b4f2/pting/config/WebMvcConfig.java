25ea45c8c9c4aa565622e84d1eb78cca578817b2
doorcs <itsdoorcs@gmail.com>
2025-09-03 16:16:29 +0900

    feat: 로그인 argument resolver 구현

--- src/main/java/com/b4f2/pting/config/WebMvcConfig.java
+++ src/main/java/com/b4f2/pting/config/WebMvcConfig.java
@@ -1,11 +1,19 @@
 package com.b4f2.pting.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import lombok.RequiredArgsConstructor;
+
 @Configuration
+@RequiredArgsConstructor
 public class WebMvcConfig implements WebMvcConfigurer {
+
+    private final LoginArgumentResolver loginArgumentResolver;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -14,5 +22,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("*")
             .allowCredentials(true);
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(loginArgumentResolver);
     }
 }
