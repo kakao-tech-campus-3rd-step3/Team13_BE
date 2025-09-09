2f78c39fd8e8c8fd73c7eceec274f26d57ff6e79
ming0511 <alsrbdhkalsw@naver.com>
2025-09-09 17:09:27 +0900

    feat: MemberController에 selectSchool 메서드 추가

--- src/main/java/com/b4f2/pting/controller/MemberController.java
+++ src/main/java/com/b4f2/pting/controller/MemberController.java
@@ -2,6 +2,7 @@ package com.b4f2.pting.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +15,9 @@ import com.b4f2.pting.config.Login;
 import com.b4f2.pting.domain.Member;
 import com.b4f2.pting.dto.CertificationRequest;
 import com.b4f2.pting.dto.CertificationResponse;
+import com.b4f2.pting.dto.SchoolResponse;
 import com.b4f2.pting.service.CertificationService;
+import com.b4f2.pting.service.SchoolService;
 
 @RestController
 @RequestMapping("/api/v1/members")
@@ -22,6 +25,7 @@ import com.b4f2.pting.service.CertificationService;
 public class MemberController {
 
     private final CertificationService certificationService;
+    private final SchoolService schoolService;
 
     @PostMapping("/me/certification")
     public ResponseEntity<String> requestCertification(
@@ -43,5 +47,11 @@ public class MemberController {
     public ResponseEntity<CertificationResponse> checkCertification(@Login Member member) {
         CertificationResponse certificationResponse = certificationService.checkCertification(member);
         return ResponseEntity.ok(certificationResponse);
+    }
+
+    @PostMapping("/me/school/{schoolId}")
+    public ResponseEntity<SchoolResponse> selectSchool(@Login Member member, @PathVariable Long schoolId) {
+        SchoolResponse response = schoolService.selectSchool(member, schoolId);
+        return ResponseEntity.ok(response);
     }
 }
