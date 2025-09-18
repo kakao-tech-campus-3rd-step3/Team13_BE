cd08fa9020b5b46e15afe7832c1d9c3d26d63a48
ming0511 <alsrbdhkalsw@naver.com>
2025-09-18 23:19:21 +0900

    refactor: 엔티티 지연 로딩(LAZY) 적용

--- src/main/java/com/b4f2/pting/domain/Game.java
+++ src/main/java/com/b4f2/pting/domain/Game.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -29,7 +30,7 @@ public class Game {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "sport_id")
     private Sport sport;
 
