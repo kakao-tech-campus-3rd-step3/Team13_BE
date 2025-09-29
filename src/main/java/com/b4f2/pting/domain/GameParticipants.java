a7507ee4bd0c842e30cb0a101883b00e57b7c4f5
ming0511 <alsrbdhkalsw@naver.com>
2025-09-29 15:03:33 +0900

    Refactor: 모집 인원 체크 로직을 GameParticipants로 이동

--- src/main/java/com/b4f2/pting/domain/GameParticipants.java
+++ src/main/java/com/b4f2/pting/domain/GameParticipants.java
@@ -9,10 +9,6 @@ public class GameParticipants {
 
     private final List<GameParticipant> participants;
 
-    public int size() {
-        return participants.size();
-    }
-
     public void validateParticipated(Member member) {
         if (!hasParticipated(member)) {
             throw new IllegalArgumentException(member.getName() + "은/는 해당 게임에 참여하지 않았습니다.");
@@ -29,5 +25,15 @@ public class GameParticipants {
         return participants.stream().anyMatch(gameParticipant ->
             gameParticipant.getMember().isEqualMember(member)
         );
+    }
+
+    public void validateCapacity(Game game) {
+        if (size() >= game.getPlayerCount()) {
+            throw new IllegalStateException("모집 인원이 마감되었습니다.");
+        }
+    }
+
+    private int size() {
+        return participants.size();
     }
 }
