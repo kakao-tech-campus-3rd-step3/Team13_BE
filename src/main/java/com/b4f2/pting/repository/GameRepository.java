1d30bdd2e35ab1e6d9cac1cd8a244f95791b3b06
sylimi2r2 <sylimi2r2@naver.com>
2025-10-15 19:21:40 +0900

    fix: JPA 상속과 네이티브 쿼리의 충돌 해결

--- src/main/java/com/b4f2/pting/repository/GameRepository.java
+++ src/main/java/com/b4f2/pting/repository/GameRepository.java
@@ -34,7 +34,7 @@ public interface GameRepository extends JpaRepository<Game, Long> {
             update game
             set game_status = 'END'
             where game_status = 'ON_MATCHING' and start_time <= :deadline
-            returning *
+            returning *, 'Game' as clazz_
         """, nativeQuery = true)
     List<Game> endMatchingGames(@Param("deadline") LocalDateTime deadLine);
 }
