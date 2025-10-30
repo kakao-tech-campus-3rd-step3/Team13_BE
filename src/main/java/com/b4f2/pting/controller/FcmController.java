3c8a1f315b39aa27e53fb42acaee918e95daa533
doorcs <itsdoorcs@gmail.com>
2025-10-30 20:10:51 +0900

    refactor: 불필요한 기능 테스트용 메서드 제거

--- src/main/java/com/b4f2/pting/controller/FcmController.java
+++ src/main/java/com/b4f2/pting/controller/FcmController.java
@@ -1,14 +1,10 @@
 package com.b4f2.pting.controller;
 
-import java.util.List;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.google.firebase.messaging.FirebaseMessagingException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -28,19 +24,5 @@ public class FcmController {
     public ResponseEntity<String> saveToken(@Login Member member, @RequestBody FcmTokenRequest request) {
         fcmService.saveToken(member, request.token());
         return ResponseEntity.ok("FCM 토큰이 저장되었습니다.");
-    }
-
-    @PostMapping("/single")
-    public void testSingleNotification() throws FirebaseMessagingException {
-        String title = "FCM 알람 테스트!";
-        String body = "테스트입니다~~";
-        fcmService.sendSinglePush("token", title, body);
-    }
-
-    @PostMapping("/multicast")
-    public void testMulticastNotification() throws FirebaseMessagingException {
-        String title = "FCM 알람 테스트!";
-        String body = "테스트입니다~~~";
-        fcmService.sendMulticastPush(List.of("token1", "token2"), title, body);
     }
 }
