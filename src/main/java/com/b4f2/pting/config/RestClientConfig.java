062031fe2ce9074ef40048a052ebc21f0b26ad33
doorcs <itsdoorcs@gmail.com>
2025-10-30 19:46:40 +0900

    refactor: timeout 설정 추가

--- src/main/java/com/b4f2/pting/config/RestClientConfig.java
+++ src/main/java/com/b4f2/pting/config/RestClientConfig.java
@@ -1,7 +1,10 @@
 package com.b4f2.pting.config;
 
+import java.time.Duration;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
 @Configuration
@@ -9,6 +12,10 @@ public class RestClientConfig {
 
     @Bean
     RestClient restClient() {
-        return RestClient.create();
+        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+        requestFactory.setConnectTimeout(Duration.ofSeconds(3));
+        requestFactory.setReadTimeout(Duration.ofSeconds(3));
+
+        return RestClient.builder().requestFactory(requestFactory).build();
     }
 }
