787a64030b5c3eb01fac529fb94a6c9eb63a68d3
doorcs <itsdoorcs@gmail.com>
2025-11-02 13:01:05 +0900

    chore: 테스트 데이터 수정

--- src/main/resources/static/sql/data.sql
+++ src/main/resources/static/sql/data.sql
@@ -1,6 +1,6 @@
-INSERT INTO sport(name, recommended_player_count)
-VALUES ('basket ball', 10),
-       ('futsal', 10);
+INSERT INTO sport(game_name, game_location, recommended_player_count)
+VALUES ('basket ball', '넉넉한 터', 10),
+       ('futsal', '넉넉한 터', 10);
 
 INSERT INTO school (name, postfix)
 VALUES ('부산대학교', 'pusan.ac.kr');
