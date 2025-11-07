d7ba5228696f1989607f7d05c9fab33a41863543
doorcs <itsdoorcs@gmail.com>
2025-11-07 17:12:53 +0900

    refactor: swagger 설정 수정
    애플리케이션 이름 추가
    요청에 JWT 토큰을 포함시킬 수 있는 기능 추가

--- src/main/java/com/b4f2/pting/config/SwaggerConfig.java
+++ src/main/java/com/b4f2/pting/config/SwaggerConfig.java
@@ -1,15 +1,49 @@
 package com.b4f2.pting.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
-        return new OpenAPI();
+        return new OpenAPI()
+                .info(apiInfo())
+                .servers(List.of(server()))
+                .components(components())
+                .addSecurityItem(securityRequirement());
+    }
+
+    private Info apiInfo() {
+        return new Info().title("P-Ting").description("P-Ting API 명세").version("1.0.0");
+    }
+
+    private Server server() {
+        return new Server().url("/api");
+    }
+
+    private Components components() {
+        return new Components()
+                .addSecuritySchemes(
+                        "JWT",
+                        new SecurityScheme()
+                                .name("JWT")
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT"));
+    }
+
+    private SecurityRequirement securityRequirement() {
+        return new SecurityRequirement().addList("JWT");
     }
 }
